ff7ca8032deaeeaa9ad4a4e484cdc0380e5c8c99
Marcos Lafoz <marcos.lafoz96@gmail.com>
2023-10-24 15:41:20 +0200

    fix: :bug:

--- EjerciciosAD/src/ejercicio1x01/Main.java
+++ EjerciciosAD/src/ejercicio1x01/Main.java
@@ -2,7 +2,7 @@ package ejercicio1x01;
 
 public class Main {
 
-	//Método que leer un fichero
+	//Método que lee un fichero
     public static void main(String[] args) {
         
         String[] linea = Services.leerArchivo("data/entrada.txt"); 
