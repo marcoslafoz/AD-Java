704d453dbe72b4432cdbd3568f7e044e94369e9e
marcoslafoz <marcos.lafoz96@gmail.com>
2023-10-30 18:37:05 +0100

    docs: :memo: Documentacion añadida en los ejercicios 1x02 y 1x03

--- EjerciciosAD/src/ejercicio1x02/Main.java
+++ EjerciciosAD/src/ejercicio1x02/Main.java
@@ -11,30 +11,37 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Definición de los nombres de archivo de entrada y salida
         final String ARCHIVO_ENTRADA = "data/entrada.txt";
         final String ARCHIVO_SALIDA = "data/estadisticas.txt";
-        
-        File archivoEntrada = new File(ARCHIVO_ENTRADA);
 
+        // Crear objetos File para los archivos de entrada y salida
+        File archivoEntrada = new File(ARCHIVO_ENTRADA);
+        File archivoSalida = new File(ARCHIVO_SALIDA);
+
+        // Verificar si el archivo de entrada existe en el sistema de archivos
         if (!archivoEntrada.exists()) {
             System.out.println("El archivo no existe en el sistema de archivos.");
             return;
         }
 
+        // Verificar si el archivo de entrada es un fichero
         if (!archivoEntrada.isFile()) {
             System.out.println("El archivo no es un fichero.");
             return;
         }
 
         try {
+            // Crear objetos para lectura y escritura de archivos
             BufferedReader br = new BufferedReader(new FileReader(archivoEntrada));
-            BufferedWriter bw = new BufferedWriter(new FileWriter(ARCHIVO_SALIDA));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(archivoSalida));
 
             String linea;
             int totalLineas = 0;
             int totalCaracteres = 0;
             int totalPalabras = 0;
 
+            // Leer líneas del archivo de entrada y calcular estadísticas
             while ((linea = br.readLine()) != null) {
                 totalLineas++;
                 int caracteres = linea.length();
@@ -43,20 +50,21 @@ public class Main {
                 totalCaracteres += caracteres;
                 totalPalabras += palabras;
 
+                // Escribir estadísticas en el archivo de salida
                 bw.write("Línea " + totalLineas + ": " + caracteres + " caracteres y " + palabras + " palabras");
                 bw.newLine();
             }
 
+            // Escribir estadísticas totales en el archivo de salida
             bw.write("Total: " + totalLineas + " líneas, " + totalCaracteres + " caracteres y " + totalPalabras + " palabras");
 
+            // Cerrar los objetos de lectura y escritura de archivos
             br.close();
             bw.close();
 
-            System.out.println("El archivoEntrada de estadísticas se ha creado con éxito.");
+            System.out.println("El archivo de estadísticas se ha creado con éxito.");
         } catch (IOException e) {
-            System.err.println("Error al procesar el archivoEntrada: " + e.getMessage());
+            System.err.println("Error al procesar el archivo de entrada: " + e.getMessage());
         }
-
-        
     }
 }
--- EjerciciosAD/src/ejercicio1x03/Utils.java
+++ EjerciciosAD/src/ejercicio1x03/Utils.java
@@ -8,10 +8,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Utils {
-	public static void escribirFrasesEnFichero(String archivo, Scanner scanner) {
+    // Método para escribir frases en un archivo de texto
+    public static void escribirFrasesEnFichero(String archivo, Scanner scanner) {
         try {
+            // Abre un archivo en modo de adición
             BufferedWriter writer = new BufferedWriter(new FileWriter(archivo, true));
 
+            // Solicita al usuario que escriba frases hasta que ingrese '***'
             System.out.println("Escribe frases en el fichero. Para detener la escritura, ingresa '***' en una línea nueva.");
             String frase;
             while (true) {
@@ -19,36 +22,46 @@ public class Utils {
                 if (frase.equals("***")) {
                     break;
                 }
+                // Escribe la frase en el archivo y agrega una nueva línea
                 writer.write(frase);
                 writer.newLine();
             }
 
+            // Cierra el archivo y muestra un mensaje de éxito
             writer.close();
             System.out.println("Las frases se han escrito en el archivo.");
         } catch (IOException e) {
+            // Manejo de errores de E/S
             System.err.println("Error al escribir en el archivo: " + e.getMessage());
         }
     }
 
+    // Método para leer frases de un archivo de texto y mostrarlas en la consola
     public static void leerFrasesDelFichero(String archivo) {
         try {
+            // Abre el archivo para lectura
             BufferedReader reader = new BufferedReader(new FileReader(archivo));
             String linea;
             int numeroLinea = 1;
 
+            // Lee y muestra cada línea del archivo
             while ((linea = reader.readLine()) != null) {
                 System.out.println("Línea " + numeroLinea + ": " + linea);
                 numeroLinea++;
             }
 
+            // Cierra el archivo
             reader.close();
         } catch (IOException e) {
+            // Manejo de errores de E/S
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
     }
 
+    // Método para consultar estadísticas sobre un archivo de texto
     public static void consultarEstadisticas(String archivo) {
         try {
+            // Abre el archivo para lectura
             BufferedReader reader = new BufferedReader(new FileReader(archivo));
             int numeroLineas = 0;
             int numeroPalabras = 0;
@@ -56,27 +69,33 @@ public class Utils {
             String linea;
             while ((linea = reader.readLine()) != null) {
                 numeroLineas++;
+                // Divide la línea en palabras y suma el total de palabras
                 numeroPalabras += linea.split("\\s+").length;
             }
 
+            // Muestra el número de líneas y palabras
             System.out.println("Número de líneas: " + numeroLineas);
             System.out.println("Número de palabras: " + numeroPalabras);
             reader.close();
         } catch (IOException e) {
+            // Manejo de errores de E/S
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
     }
 
+    // Método para buscar una palabra en un archivo de texto
     public static void buscarPalabraEnFichero(String archivo, Scanner scanner) {
         System.out.print("Introduce una palabra para buscar en el archivo: ");
         String palabra = scanner.nextLine();
 
+        // Verifica si la palabra contiene espacios en blanco
         if (palabra.contains(" ")) {
             System.out.println("La palabra no puede contener espacios en blanco.");
             return;
         }
 
         try {
+            // Abre el archivo para lectura
             BufferedReader reader = new BufferedReader(new FileReader(archivo));
             String linea;
             int numeroLinea = 1;
@@ -85,6 +104,7 @@ public class Utils {
             while ((linea = reader.readLine()) != null) {
                 if (linea.contains(palabra)) {
                     encontrada = true;
+                    // Muestra la línea en la que se encontró la palabra y sale del bucle
                     System.out.println("La palabra está en la línea " + numeroLinea + " del fichero de texto.");
                     break;
                 }
@@ -95,52 +115,65 @@ public class Utils {
                 System.out.println("La palabra no está en el fichero de texto.");
             }
 
+            // Cierra el archivo
             reader.close();
         } catch (IOException e) {
+            // Manejo de errores de E/S
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
     }
 
+    // Método para convertir frases a mayúsculas y escribirlas en un nuevo archivo
     public static void convertirFrasesAMayusculas(String archivo) {
         try {
+            // Abre el archivo para lectura y crea un nuevo archivo para escritura
             BufferedReader reader = new BufferedReader(new FileReader(archivo));
             BufferedWriter writer = new BufferedWriter(new FileWriter("data/mayusculas.txt"));
 
             String linea;
             while ((linea = reader.readLine()) != null) {
+                // Convierte la línea a mayúsculas y la escribe en el nuevo archivo
                 writer.write(linea.toUpperCase());
                 writer.newLine();
             }
 
+            // Cierra ambos archivos y muestra un mensaje de éxito
             reader.close();
             writer.close();
             System.out.println("Las frases en mayúsculas se han escrito en el archivo mayusculas.txt.");
         } catch (IOException e) {
+            // Manejo de errores de E/S
             System.err.println("Error al convertir y escribir en el archivo mayusculas.txt: " + e.getMessage());
         }
     }
 
+    // Método para convertir frases a minúsculas y escribirlas en un nuevo archivo
     public static void convertirFrasesAMinusculas(String archivo) {
         try {
+            // Abre el archivo para lectura y crea un nuevo archivo para escritura
             BufferedReader reader = new BufferedReader(new FileReader(archivo));
             BufferedWriter writer = new BufferedWriter(new FileWriter("data/minusculas.txt"));
 
             String linea;
             while ((linea = reader.readLine()) != null) {
+                // Convierte la línea a minúsculas y la escribe en el nuevo archivo
                 writer.write(linea.toLowerCase());
                 writer.newLine();
             }
 
+            // Cierra ambos archivos y muestra un mensaje de éxito
             reader.close();
             writer.close();
             System.out.println("Las frases en minúsculas se han escrito en el archivo minusculas.txt.");
         } catch (IOException e) {
+            // Manejo de errores de E/S
             System.err.println("Error al convertir y escribir en el archivo minusculas.txt: " + e.getMessage());
         }
     }
-    
+
+    // Método para mostrar un menú de opciones al usuario
     public static void mostrarOpciones() {
-    	 System.out.println("Menú de opciones:");
+         System.out.println("Menú de opciones:");
          System.out.println("0) Salir del programa.");
          System.out.println("1) Escribir varias frases en el fichero de texto.");
          System.out.println("2) Leer todas las frases del fichero de texto.");
